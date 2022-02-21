9569c60318fe5209868e6d80e2eb2ce3cac9b698
Kai Zhang <kaizhang@macbook-pro.myfiosgateway.com>
2022-02-21 15:56:50 -0500

    added the header component and links to home, shop page and contact

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SHOP_DATA from "./shop.data";
 import CollectionPreview from '../../components/preview-collection/preview-collection.component'
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
 class ShopPage extends React.Component{
     constructor(props){
